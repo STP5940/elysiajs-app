ddfdf9d77111e8ddc4faa734ef93383b884275e7
STP5940 <sitthipong114@gmail.com>
2025-11-14 14:06:25 +0700

    👷 build: Test deploy on vercel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-// src/index.ts
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia()
-    .get('/', () => 'Hello Vercel Function');
-
-export default app;
+export default new Elysia()
+    .get('/', () => 'Hello Vercel Function')
+    .post('/', ({ body }) => body, {
+        body: t.Object({
+            name: t.String()
+        })
+    })
